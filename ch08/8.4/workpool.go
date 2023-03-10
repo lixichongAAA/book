d4c7d6c7b71a24635a4dc9e63b291244c9dfd3bb
Lxc <1104426357@qq.com>
2023-03-10 15:30:52 +0800

    modify

--- ch08/8.4/workpool.go
+++ ch08/8.4/workpool.go
@@ -13,9 +13,10 @@ type Task struct {
 type Job struct {
 	Task Task
 }
+
 var (
 	MaxWorker = 5
-	JobQueue chan Job //工作通道，模拟需要处理的所有工作
+	JobQueue  chan Job //工作通道，模拟需要处理的所有工作
 )
 
 type Worker struct {
@@ -26,7 +27,7 @@ type Worker struct {
 }
 
 func NewWorker(workerPool chan chan Job, id int) Worker {
-	fmt.Printf("new a worker(%d)\n",id)
+	fmt.Printf("new a worker(%d)\n", id)
 	return Worker{
 		id:         id,
 		WorkerPool: workerPool, //workerPool和scheduler的是同一个
@@ -35,7 +36,7 @@ func NewWorker(workerPool chan chan Job, id int) Worker {
 	}
 }
 
-//监听任务和结束信号
+// 监听任务和结束信号
 func (w Worker) Start() {
 	go func() {
 		for {
@@ -43,12 +44,12 @@ func (w Worker) Start() {
 			w.WorkerPool <- w.JobChannel
 			fmt.Println("register private JobChannel to public WorkerPool", w)
 			select {
-			case job := <-w.JobChannel://收到任务
+			case job := <-w.JobChannel: //收到任务
 				fmt.Println("get a job from private w.JobChannel")
 				fmt.Println(job)
-				time.Sleep(5* time.Second)
-			case <-w.exit://收到结束信号
-				fmt.Println("worker exit",w)
+				time.Sleep(5 * time.Second)
+			case <-w.exit: //收到结束信号
+				fmt.Println("worker exit", w)
 				return
 			}
 		}
@@ -61,22 +62,22 @@ func (w Worker) Stop() {
 	}()
 }
 
-//排程中心
+// 排程中心
 type Scheduler struct {
 	WorkerPool chan chan Job //工作池
-	MaxWorkers int //最大工作者数
-	Workers []*Worker //worker队列
+	MaxWorkers int           //最大工作者数
+	Workers    []*Worker     //worker队列
 }
 
-//创建排程中心
+// 创建排程中心
 func NewScheduler(maxWorkers int) *Scheduler {
 	pool := make(chan chan Job, maxWorkers) //工作池
 	return &Scheduler{WorkerPool: pool, MaxWorkers: maxWorkers}
 }
 
-//工作池的初始化
+// 工作池的初始化
 func (s *Scheduler) Create() {
-	workers := make([]*Worker,s.MaxWorkers)
+	workers := make([]*Worker, s.MaxWorkers)
 	for i := 0; i < s.MaxWorkers; i++ {
 		worker := NewWorker(s.WorkerPool, i)
 		worker.Start()
@@ -86,17 +87,17 @@ func (s *Scheduler) Create() {
 	go s.schedule()
 }
 
-//工作池的关闭
-func (s *Scheduler) Shutdown()  {
+// 工作池的关闭
+func (s *Scheduler) Shutdown() {
 	workers := s.Workers
-	for _,w := range workers{
+	for _, w := range workers {
 		w.Stop()
 	}
 	time.Sleep(time.Second)
 	close(s.WorkerPool)
 }
 
-//排程
+// 排程
 func (s *Scheduler) schedule() {
 	for {
 		select {
@@ -121,16 +122,17 @@ func main() {
 	go createJobQueue()
 	time.Sleep(100 * time.Second)
 	scheduler.Shutdown()
-	time.Sleep(10*time.Second)
+	time.Sleep(10 * time.Second)
 }
-//创建模拟任务
+
+// 创建模拟任务
 func createJobQueue() {
 	for i := 0; i < 30; i++ {
 		task := Task{Num: 1}
 		job := Job{Task: task}
 		JobQueue <- job
 		fmt.Println("JobQueue add one job")
-		time.Sleep(1 * time.Second)
+		//time.Sleep(1 * time.Second)
 	}
 
 }
